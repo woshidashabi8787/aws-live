cc4282898078147d85dedfe247e12bf139a551e9
woshidashabi8787 <110081047+woshidashabi8787@users.noreply.github.com>
2024-04-17 21:40:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c5g8qqu2kxgq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "edwinsoo-employee"
+customregion = "us-east-1"
+
